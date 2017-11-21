d9cf8e8e7a22543575771083299b5c6894387b9e
Ricky Smith <ricky.smith@rigor.com>
2017-11-21 11:10:29 -0500

    Adjust event announcement schedule

--- events.js
+++ events.js
@@ -23,9 +23,13 @@ module.exports.daily_reminder = (event, context, callback) => {
             return flattened;
         })
         .filter((event) => {
-            return moment(event.start).diff(now, 'weeks') <= 1;
+            // If it's a slack command, send a full week
+            // Otherwise, send only the next day
+            var interval = is_from_slash_command ? 'weeks' : 'days';
+            return moment(event.start).diff(now, interval) <= 1;
         })
         .map((event) => {
+            // Construct the message attachments from events
             return {
                 title: event.summary,
                 title_link: event.url,
@@ -34,17 +38,21 @@ module.exports.daily_reminder = (event, context, callback) => {
             };
         })
         .then((attachments) => {
+            // Build the message payload
             return {
                 username: "ATV Events",
                 attachments: attachments
             };
         })
         .tap((message) => {
+            // This function is to handle function invocations that aren't through the API
+            // It will reach out to the slack API to send the message
             if (is_from_slash_command) {
                 return;
             }
 
             if (message.attachments.length == 0) {
+                console.log('There are no events to announce. Skipping message send.');
                 return;
             }
 
@@ -52,6 +60,7 @@ module.exports.daily_reminder = (event, context, callback) => {
             slack.webhook(message, () => {});
         })
         .then((message) => {
+            // Return the Slack message as API results (for slash commands)
             callback(null, {
                 statusCode: 200,
                 body: JSON.stringify(message)
